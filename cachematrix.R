da58a246deb68ea2c39420ba8e58e87601c332af
micromaker60 <37558314+micromaker60@users.noreply.github.com>
2018-03-28 10:22:52 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,21 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
+  #function that changes matrix stored in main function
   set <- function(y){
+    #resets the main function value x to the new y, hence <<-, <- would only change the value in set()
     x <<- y
+    #rests the value of the inverse stored at i to NULL, since there is a new matrix
     i <<- NULL
     
   }
+  #get() returns the matrix x stored in main function
   get <- function () x
+  #updates i with inverse matrix
   setinverse <- function(inverse) i <<- inverse
+  #gets value of i
   getinverse <- function () i
+  #stores all the functions in a list so the object assigned to makeCacheMatrix has all the functions
   list(set = set, get = get, 
        setinverse = setinverse, 
        getinverse = getinverse)
@@ -25,12 +32,16 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   i <- x$getinverse()
+  #if there is a value stored at i (there is a mean ) then return it
   if(!is.null(i)) {
     message("getting cached data")
     return(i)
   }
+  #get the matrix value stored at x
   data <- x$get()
+  #calculate inverse of matrix
   i <- solve(data, ...)
+  #set the new inverse matrix to be cached
   x$setinverse(i)
   i
 }
